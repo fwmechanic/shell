c35c3dce12134079ddae0ab217392453a626ef58
Kevin Goodwin <fwmechanic@gmail.com>
2021-11-14 19:34:00 -0700

    improvements, bugfix, factor out to module

--- MyMods/StmtToCsv.pm
+++ MyMods/StmtToCsv.pm
@@ -18,3 +18,16 @@ sub cents_to_dc { my ($txcents) = @_;
 sub showtxn { my ($holder,$dt,$txcents,$desc) = @_;
    printf "%-17s: %s %s %s\n", $holder, $dt, cents_to_dc($txcents), $desc;
    }
+
+sub cross_chk_totals { my ($stmtTotal,$accumdTxns,$anno) = @_;
+   $anno = sprintf "%-10s", $anno;
+   if( $stmtTotal != $accumdTxns ) {
+      printf "**************************************************************************************\n";
+      printf "$anno: stmtTotal (%s) != accumdTxns (%s) DIFFER by %s !!!\n", MyMods::StmtToCsv::cents_to_dc($stmtTotal), MyMods::StmtToCsv::cents_to_dc($accumdTxns), MyMods::StmtToCsv::cents_to_dc($stmtTotal - $accumdTxns);
+      printf "**************************************************************************************\n";
+      exit(1);
+      }
+   else {
+      printf "$anno: stmtTotal (%s) == accumdTxns (%s) same\n", MyMods::StmtToCsv::cents_to_dc($stmtTotal), MyMods::StmtToCsv::cents_to_dc($accumdTxns);
+      }
+   }
